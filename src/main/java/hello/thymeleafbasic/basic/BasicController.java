7871931331e2279aeb7de0f97203d56c063385d3
fbehddn <tom0611@naver.com>
2023-05-17 13:14:33 +0900

    주석

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -120,6 +120,12 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
     @Data
     public class User {
         private String username;
